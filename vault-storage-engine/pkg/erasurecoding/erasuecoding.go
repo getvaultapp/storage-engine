d0159bcada57de313f5e394d5f307a52f3e5e9e5
tnxl <tosinoyinboblessed@gmail.com>
2025-03-16 17:48:55 +0100

    UPDATE: Removed excess data in retrieved files

--- vault-storage-engine/pkg/erasurecoding/erasuecoding.go
+++ vault-storage-engine/pkg/erasurecoding/erasuecoding.go
@@ -1,56 +1,3 @@
-/* package erasurecoding
-
-import (
-	"fmt"
-
-	"github.com/klauspost/reedsolomon"
-)
-
-const (
-	DataShards   = 8
-	ParityShards = 6
-)
-
-// Encode encodes data using Reed-Solomon erasure coding
-func Encode(data []byte) ([][]byte, error) {
-	enc, err := reedsolomon.New(DataShards, ParityShards)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create encoder: %w", err)
-	}
-
-	shards, err := enc.Split(data)
-	if err != nil {
-		return nil, fmt.Errorf("failed to split data into shards: %w", err)
-	}
-
-	err = enc.Encode(shards)
-	if err != nil {
-		return nil, fmt.Errorf("failed to encode data: %w", err)
-	}
-
-	return shards, nil
-}
-
-// Decode decodes data using Reed-Solomon erasure coding
-func Decode(shards [][]byte) ([]byte, error) {
-	enc, err := reedsolomon.New(DataShards, ParityShards)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create decoder: %w", err)
-	}
-
-	err = enc.Reconstruct(shards)
-	if err != nil {
-		return nil, fmt.Errorf("failed to reconstruct data: %w", err)
-	}
-
-	data, err := enc.Join(nil, shards, len(shards[0])*DataShards)
-	if err != nil {
-		return nil, fmt.Errorf("failed to join shards: %w", err)
-	}
-
-	return data, nil
-} */
-
 package erasurecoding
 
 import (
@@ -95,5 +42,7 @@ func Decode(shards [][]byte) ([]byte, error) {
 		return nil, err
 	}
 
-	return buf.Bytes(), nil
+	//return buf.Bytes(), nil
+
+	return bytes.Trim(buf.Bytes(), "\x00"), nil
 }
